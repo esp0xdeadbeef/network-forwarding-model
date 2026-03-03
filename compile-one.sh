f67c0bc5ecd9a68316757551164a5fdfa18536b8
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-03 22:38:15 +0100

    changed default compile-one, and added outputs.

--- compile-one.sh
+++ compile-one.sh
@@ -1,3 +1,3 @@
-#nix run .#compile-and-solve -- ../network-compiler/examples/overlay-east-west/inputs.nix
+nix run .#compile-and-solve -- ../network-compiler/examples/overlay-east-west/inputs.nix
 #nix run .#compile-and-solve -- ../network-compiler/examples/multi-wan/inputs.nix
-nix run .#compile-and-solve -- ../network-compiler/examples/single-wan/inputs.nix
+#nix run .#compile-and-solve -- ../network-compiler/examples/single-wan/inputs.nix
