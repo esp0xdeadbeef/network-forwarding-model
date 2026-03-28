ceeccf3a55a3ed5efe69d00a1912c92bfa578cb6
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-28 20:53:28 +0100

    updated test files

--- compile-all-examples.sh
+++ compile-all-examples.sh
@@ -7,7 +7,8 @@ cat >"$GIT_CONFIG_GLOBAL" <<'EOF'
 [safe]
   directory = *
 EOF
-example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+#example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+example_repo=$(nix flake prefetch github:esp0xdeadbeef/network-labs --json | jq -r .storePath)
 
 find "$example_repo/examples" -name 'intent.nix' -type f -exec sh -c '
   printf "\n\n%s:\n\n" "$1"
--- compile-one.sh
+++ compile-one.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 #nix run .#compile-and-solve -- ../network-compiler/examples/single-wan-with-nebula/inputs.nix
 #nix run .#compile-and-solve -- ../network-compiler/examples/priority-stability/inputs.nix
 #nix run .#compile-and-solve -- ../network-compiler/examples/overlay-east-west/inputs.nix
-example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+example_repo=$(nix flake prefetch github:esp0xdeadbeef/network-labs --json | jq -r .storePath)
 
-nix run .#compile-and-build-forwarding-model -- "$example_repo/examples/overlay-east-west/intent.nix"
+nix run .#compile-and-build-forwarding-model -- "$example_repo/examples/single-wan-with-nebula-any-to-any-fw/intent.nix"
